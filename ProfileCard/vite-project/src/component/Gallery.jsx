b9655c320c71d7f6bd3932a85869b370d8f55272
Jitendra Kumar Chauhan <jitendra.chauhan@abes.ac.in>
2026-01-29 10:24:55 +0530

    Profile Card

--- ProfileCard/vite-project/src/component/Gallery.jsx
+++ ProfileCard/vite-project/src/component/Gallery.jsx
@@ -5,7 +5,7 @@ function Gallery() {
     
   return (
     <div>
-    <Profile barcode="./barcode.png" pic="./abes.png"  pic1="./student.png" desc="ABES Engineering College  NH24 Ghaziabad" name="Jitendra Kumar Chauhan" IDN="20" CardN="2026123" StartD="28/01/2026"/>
+    <Profile barcode="./barcode.png" pic="./abes.png"  pic1="./student.png" desc="ABES Engineering College  NH24 Ghaziabad" name="Jitendra Kumar Chauhan" IDN="2026" CardN="2026123" StartD="28/01/2026"/>
         
         
         </div>
--- ProfileCard/vite-project/src/component/Profile.jsx
+++ ProfileCard/vite-project/src/component/Profile.jsx
@@ -1,46 +1,58 @@
 import React from "react";
 import "./profile.css";
+import barcode from "./barcode.png";
+import student from "./student.png";
 
 function Profile(props) {
   return (
     <>
-    <div className="outer">
-      <img src={props.pic} alt="" height={100} width={100} />
-      <h1>{props.desc}</h1>
+      <div className="outer">
+        <div className="header">
+          <img src={props.pic} alt="" height={100} width={100} />
 
+          <h1>{props.desc}</h1>
+        </div>
+        <div className="inner">
+          <div className="box">
+            <div className="innerbox">
+              <h3>
+                Name:<br></br>
+                {props.name}
+              </h3>
+              <br />
+              <h3>
+                Student ID Number:<br></br>
+                {props.IDN}
+              </h3>
+              <br />
+              <h3>
+                Card Number:<br></br>
+                {props.CardN}
+              </h3>
+              <br />
+              <h3>
+                Start Date :<br></br>
+                {props.StartD}
+              </h3>
+            </div>
 
-      <div className="inner">
-        <div className="box">
-                <div className="jeet">
-        <h3>
-          Name:<br></br>
-          {props.name}
-        </h3>
-        <br></br>
-        <h3>
-          Student ID Number:<br></br>
-          {props.IDN}
-        </h3>
-        <h3>
-          Card Number:<br></br>
-          {props.CardN}
-        </h3>
-        <h3>
-          Start Date :<br></br>
-          {props.StartD}
-        </h3>
+            <div className="barcode">
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <button>Student</button>
+              <br />
+              <br />
+              <img src={barcode} alt="barcode" />
+            </div>
+          </div>
+          <div className="spic">
+            <img src={student} alt="not to display" height={300} width={200} />
+          </div>
+        </div>
       </div>
-
-      <div className="barcode">
-        <button>Student</button>
-        <img src={props.barcode} alt="barcode" />
-      </div>
-</div>
-      <div>
-        <img src={props.pic1} alt="not to display"  height={100} width={100}/>
-      </div>
-    </div>
-    </div>
     </>
   );
 }
